578c8f979f024f36061867749a1be3abcb91bf1f
Tenghuy <tenghuyly35@gmailcom>
2025-01-13 17:31:56 +0700

    new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -62,9 +62,31 @@ const Contact = () => {
             <div className='col-span-12 lg:col-span-5'>
               <h1 className='text-[#A4CA62] text-[18px] font-[700]'>Keep In Touch</h1>
               <div className='flex flex-col space-y-4 text-[14px] text-[#415464]'>
-                <p>Borey Chip Mong 271, #117, St Commercial, Sangkat Chak Agre Leu,Khan Mean Chey, Phnom Penh, Cambodia</p>
-                <a href='#'>info@egecambodia.com</a>
-                <a href='#'>(+855) 078 256 088</a>
+              <div className='flex items-start space-x-1'>
+                <div>
+                  <svg className="w-6 h-6 text-[#415464]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z" />
+                  </svg>
+                </div>
+                <p className='p-[2px]'>Borey Chip Mong 271, #117, St Commercial, Sangkat Chak Agre Leu,Khan Mean Chey, Phnom Penh, Cambodia</p>
+              </div>
+              <div className='flex items-start space-x-1'>
+                <div>
+                  <svg className="w-6 h-6 text-[#415464]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" strokeLinecap="round" strokeWidth="1.5" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                  </svg>
+                </div>
+                <a href='mailto:info@egecambodia.com' className='p-[2px]'>info@egecambodia.com</a>
+              </div>
+              <div className='flex items-start space-x-1'>
+                <div>
+                  <svg className="w-6 h-6 text-[#415464]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z" />
+                  </svg>
+                </div>
+                <a href='#' className='p-[2px]'>(+855) 078 256 088</a>
+              </div>
               </div>
 
               <div className="map-container mt-4">
